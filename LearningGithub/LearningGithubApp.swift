0cd9e10ed71fb0c999128654065a2fbe8d109034
Matthew Escobido <matthew.escobido@gmail.com>
2024-02-23 14:14:22 +0800

    Change greeting highlighting health

---
+++ LearningGithub/LearningGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGithubApp.swift
+//  LearningGithub
+//
+//  Created by firstblock on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
